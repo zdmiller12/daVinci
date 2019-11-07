439ee667b898aa56719ceb2077c409287a28f955
miller <miller@torc.ai>
2019-11-06 20:31:55 -0500

    final commit before potentially breaking changes to package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reps',
+      version='2019.11.06',
+      description='Tool for optimization and plotting of Repairable Equipment Population Systems (REPS)',
+      url='https://github.com/zdmiller12/reps',
+      author='zdmiller12',
+      author_email='zdmiller12@gmail.com',
+      license='MIT',
+      packages=['reps'],
+      zip_safe=False)
